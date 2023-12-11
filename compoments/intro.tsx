39f371984e36172b78f1cc5d02775bb0fabde989
Nan <caonanla@gmail.com>
2023-12-11 23:06:29 +0100

    add introduction

---
+++ compoments/intro.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/nan_cao_photo.jpg"
+              alt="nan_cao"
+              width={"192"}
+              height={"192"}
+              quality={"95"}
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 -translate-y-[1rem] text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5]"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hi, I'm Nan. </span>I am a
+        <span className="font-bold"> junior frontend developer</span>, live with
+        my family in Hamburg Germany. I enjoy creating{" "}
+        <span className="italic"> websites and apps.</span> My focus is on{" "}
+        <span className="underline">JavaScript and React</span>
+      </motion.p>
+    </section>
+  );
+}
