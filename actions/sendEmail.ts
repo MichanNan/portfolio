07939e2ef3781671daae3c215698b65ee758cbeb
Nan <caonanla@gmail.com>
2023-12-15 14:57:57 +0100

    add send email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,4 +1,34 @@
 "use server";
+
+import { validateString } from "@/lib/utils";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.REDEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("running on the server");
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+  if (!validateString(senderEmail, 50)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 500)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+  try {
+    await resend.emails.send({
+      from: "onbording@resend.dev",
+      to: "caonanla@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error,
+    };
+  }
 };
